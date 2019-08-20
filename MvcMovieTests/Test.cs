49c4eb4a6a76d2adacb53253ad3caf57464a6d5b
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-08-20 14:43:47 -0700

    update helloWorld route and view

---
+++ MvcMovieTests/Test.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace MvcMovie.Controllers
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+            HelloWorldController uut = new HelloWorldController();
+            Assert.AreEqual("don't know yet", uut.Welcome("Ngan", 2));
+        }
+    }
+}

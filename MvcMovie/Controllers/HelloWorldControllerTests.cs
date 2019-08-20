e512ffd019e045a7770492260cc13ffe93c3ca67
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-08-20 12:40:23 -0700

    add NUnit testing package

---
+++ MvcMovie/Controllers/HelloWorldControllerTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace MvcMovie.Controllers
+{
+    [TestFixture]
+    public class HelloWorldControllerTests
+    {
+        [Test]
+        public void WelcomeTests()
+        {
+            HelloWorldController uut = new HelloWorldController();
+            Assert.AreEqual("don't know yet", uut.Welcome("Ngan", 2));
+        }
+    }
+}

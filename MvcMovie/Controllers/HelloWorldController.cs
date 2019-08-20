d98809b4eb7c3d366139cbb2ef48f9e4c8119e38
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-08-20 14:49:48 -0700

    add Welcome view

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -14,11 +14,16 @@ namespace MvcMovie.Controllers
         }
 
         // 
-        // GET: /HelloWorld/Welcome/ 
+        // GET: /HelloWorld/Welcome/
+        // The ViewData dictionary object contains data that will be passed to the view
+        // MVC model binding system automatically maps the names parameters(name and numTimes) from query string in the address bar to paramters in the Welcome method
 
-        public string Welcome(string name, int ID = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
